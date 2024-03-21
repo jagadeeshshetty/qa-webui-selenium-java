62756624b2c9c11162ebbd6fabee28bf1c561a1f
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2024-03-21 17:32:43 +0530

    update

---
+++ src/main/java/com/jc/playground/DoubleNum.java
@@ -0,0 +1,15 @@
+package com.jc.playground;
+
+public class DoubleNum {
+    public static void main(String[] args) {
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+        int number = 5;
+        int doubled = doubleNumber(number);
+        System.out.println("The doubled number is: " + doubled);
+    }
+
+    public static int doubleNumber(int input) {
+        return input * 2;
+    }
+}
