b5d8bc2dd892e48f7d7c848962a9382ca48a822e
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2024-04-28 10:16:17 +0530

    Get

--- src/test/java/org/api/Get.java
+++ src/test/java/org/api/Get.java
@@ -1,10 +1,24 @@
 package org.api;
 
+import org.testng.annotations.Test;
+
 import io.github.cdimascio.dotenv.Dotenv;
+import io.restassured.RestAssured;
 
 public class Get {
-  public static void main(String[] args) {
+
+  @Test
+  void testGetWorkspacesSuccess() {
     Dotenv env = Dotenv.load();
-    System.out.println(env.get("EMAIL"));
+    RestAssured
+        .given()
+        .baseUri("https://api.postman.com")
+        .header("X-Api-Key", env.get("POSTMAN_API_KEY"))
+        .when()
+        .get("/workspaces")
+        .then()
+        .log().all()
+        .assertThat()
+        .statusCode(200);
   }
 }
